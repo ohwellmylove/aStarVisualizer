cee19df352336450bfd691d831ede94039084d48
ohwellmylove <maowens66@gmail.com>
2021-10-14 11:47:27 -0400

    Formatted code and removed unused portions

--- astar.py
+++ astar.py
@@ -8,8 +8,6 @@ pygame.display.set_caption("A* Pathfinder Algorithm")
 
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
-BLUE = (0, 255, 0)
-YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 PURPLE = (128, 0, 128)
